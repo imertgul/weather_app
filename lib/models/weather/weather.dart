18f7b23dfbf684389022959fb7d7fe8dfb6c48c4
Mert Gül <ibrahimmertgul@gmail.com>
2021-11-18 19:34:57 +0300

    Squashed commit of the following:
    commit 4a3729e01b5850f077f8d11860e2ba56aa3c2652
    Author: Mert Gül <ibrahimmertgul@gmail.com>
    Date:   Thu Nov 18 17:39:16 2021 +0300
    
        weather model and json serialition
    
    commit 9c7d929a601a6eca8a33f9eea369bcf5e64f5e80
    Author: Mert Gül <ibrahimmertgul@gmail.com>
    Date:   Thu Nov 18 17:19:20 2021 +0300
    
        current weather data tested

---
+++ lib/models/weather/weather.dart
@@ -0,0 +1,26 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
+class Weather {
+  final String title;
+  final String description;
+  final double temp;
+  @JsonKey(name: 'feels_like')
+  final double feelsLike;
+  @JsonKey(name: 'temp_min')
+  final double tempMin;
+  @JsonKey(name: 'temp_max')
+  final double tempMax;
+  @JsonKey(name: 'speed')
+  final double windSpeed;
+
+  Weather(this.title, this.description, this.temp, this.feelsLike, this.tempMin,
+      this.tempMax, this.windSpeed);
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherToJson(this);
+}
