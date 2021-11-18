a4733e13cca9b26425a3da8d63912027ca1d37cf
Mert Gül <ibrahimmertgul@gmail.com>
2021-11-18 19:34:10 +0300

    test

--- lib/app/wheather/weather_app.dart
+++ lib/app/wheather/weather_app.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/app/wheather/add_city/add_city.dart';
 import 'package:weather_app/helpers/weather_helper.dart';
+import 'package:weather_app/repository/weather_repository.dart';
 
 class WeatherApp extends StatelessWidget {
   final String title;
-  const WeatherApp({Key? key,required this.title}) : super(key: key);
+  const WeatherApp({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    WeatherHelper.currentWeather('Kadikoy');
+    // WeatherHelper.currentWeather('Kadikoy');
+    WeatherRepository();
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -20,7 +22,7 @@ class WeatherApp extends StatelessWidget {
                     MaterialPageRoute(
                       builder: (context) => AddCity(
                         selectedCity: (city) {
-                          print(city);
+                          WeatherRepository().addCity(city);
                         },
                       ),
                     ));
@@ -28,7 +30,16 @@ class WeatherApp extends StatelessWidget {
               icon: const Icon(Icons.add)),
         ],
       ),
-      body: const Text('Weathers will be listed here'),
+      body: StreamBuilder<List<String>>(
+        stream: WeatherRepository().weathersStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return ListView(
+              children: snapshot.data!.map((e) => Text(e)).toList(),
+            );
+          } return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
--- lib/helpers/weather_helper.dart
+++ lib/helpers/weather_helper.dart
@@ -4,12 +4,12 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app/models/weather/weather.dart';
 
 class WeatherHelper {
-  static Future<void> currentWeather(String city) async {
+  static Future<Weather> currentWeather(String city) async {
     var response = await http.get(Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=b852e82532b0212520f11ba94eb9ca2f'));
     if (response.statusCode == 200) {
       Map<String, dynamic> data = jsonDecode(response.body);
-      print(Weather.fromJson(data).toJson());
-    }
+      return Weather.fromJson(data);
+    } throw StateError('Error while requesting current weather');
   }
 }
