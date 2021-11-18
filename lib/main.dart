70b1d0ede32df041c0a4a090a2cbc805ac5b9e0f
Mert Gül <ibrahimmertgul@gmail.com>
2021-11-18 16:50:48 +0300

    cities can be searchable

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AddCity(),
+      home: AddCity(selectedCity: (city){print(city);},),
     );
   }
 }
