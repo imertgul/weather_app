ac145116391664a2e4cd5a5c6f77a68cdd6276ed
İbrahim Mert Gül <ibrahimmertgul@gmail.com>
2021-11-19 23:02:41 +0300

    Feature/dockerize (#4)
    * docker file imported
    
    * reformat files fix colors

--- lib/helpers/weather_helper.dart
+++ lib/helpers/weather_helper.dart
@@ -4,10 +4,12 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app/helpers/location_helper.dart';
 import 'package:weather_app/models/weather/weather.dart';
 
+const apiKey = 'b852e82532b0212520f11ba94eb9ca2f';
+
 class WeatherHelper {
   static Future<Weather> currentWeather(String city) async {
     var response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=b852e82532b0212520f11ba94eb9ca2f'));
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey'));
     if (response.statusCode == 200) {
       Map<String, dynamic> data = jsonDecode(response.body);
       return Weather.fromJson(data);
@@ -17,7 +19,7 @@ class WeatherHelper {
 
   static Future<Weather> currentWeatherByCoord(String lat, String lon) async {
     var response = await http.get(Uri.parse(
-        'api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=b852e82532b0212520f11ba94eb9ca2f'));
+        'api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey'));
     if (response.statusCode == 200) {
       Map<String, dynamic> data = jsonDecode(response.body);
       return Weather.fromJson(data);
@@ -27,7 +29,7 @@ class WeatherHelper {
   static Future<Weather> currentWeatherByMyCoord() async {
     var locationData = await LocationHelper.getLocation();
     var response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${locationData.latitude}&units=metric&lon=${locationData.longitude}&appid=b852e82532b0212520f11ba94eb9ca2f'));
+        'https://api.openweathermap.org/data/2.5/weather?lat=${locationData.latitude}&units=metric&lon=${locationData.longitude}&appid=$apiKey'));
     if (response.statusCode == 200) {
       Map<String, dynamic> data = jsonDecode(response.body);
       return Weather.fromJson(data);
