31952b50a7e00cde7137f1982882a5c570a241c7
Mert Gül <ibrahimmertgul@gmail.com>
2021-11-21 20:27:06 +0300

    Update auth_repository.dart

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -19,16 +19,13 @@ class AuthRepository {
   final StreamController<AuthState> _authStateController = StreamController();
 
   AuthRepository._() {
-    //you can save your token to storage and change the initial state by token if you want
     _authStateController.add(AuthRequired());
   }
 
   Stream<AuthState> get authState => _authStateController.stream;
 
-
   Future<void> configureAmplify() async {
     // Add Pinpoint and Cognito Plugins, or any other plugins you want to use
-    // AmplifyAnalyticsPinpoint analyticsPlugin = AmplifyAnalyticsPinpoint();
     AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
     await Amplify.addPlugins([authPlugin]);
 
@@ -36,16 +33,42 @@ class AuthRepository {
       // Once Plugins are added, configure Amplify
       // Note: Amplify can only be configured once.
       await Amplify.configure(amplifyconfig);
+      Amplify.Auth.fetchAuthSession().then((session) {
+        if (session.isSignedIn) {
+          _authStateController.add(AuthSuccess());
+        }
+      });
+      Amplify.Hub.listen([HubChannel.Auth], (hubEvent) {
+        switch (hubEvent.eventName) {
+          case "SIGNED_IN":
+            {
+              _authStateController.add(AuthSuccess());
+            }
+            break;
+          case "SIGNED_OUT":
+            {
+              _authStateController.add(AuthRequired());
+            }
+            break;
+          case "SESSION_EXPIRED":
+            {
+              _authStateController.add(AuthRequired());
+            }
+            break;
+          default: _authStateController.add(AuthRequired());
+        }
+      });
     } on AmplifyAlreadyConfiguredException {
       StateError(
           "Tried to reconfigure Amplify; this can occur when your app restarts on Android.");
     }
   }
 
-  Future<bool> register({required String email ,required String password}) async {
-     Map<String, String> _userAttributes = {
-    'email': email,
-  };
+  Future<bool> register(
+      {required String email, required String password}) async {
+    Map<String, String> _userAttributes = {
+      'email': email,
+    };
     SignUpResult res = await Amplify.Auth.signUp(
         username: email,
         password: password,
@@ -66,9 +89,13 @@ class AuthRepository {
       username: email,
       password: password,
     );
-    if (res.isSignedIn) {
-      _authStateController.add(AuthSuccess());
-    }
+    // if (res.isSignedIn) {
+    //   _authStateController.add(AuthSuccess());
+    // }
     return res.isSignedIn;
+  }
+
+  Future<void> signOut() async {
+    await Amplify.Auth.signOut();
   }
 }
